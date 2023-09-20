7c13c4cea1df48da6d932c9b2b4d2629747939f4
alekseiveprev <72622572+alekseiveprev@users.noreply.github.com>
2023-09-20 12:12:13 +0300

    feat: button task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,65 @@
+import { useState } from 'react';
 import { FC } from 'react';
-
 import './style.css';
 
+type FilterNype = 'all' | 'ruble' | 'dollar';
+
 export const App: FC<{ name: string }> = ({ name }) => {
+  const [money, setMoney] = useState([
+    { banknote: 'dollar', nominal: 100, number: 'a123456789' },
+    { banknote: 'dollar', nominal: 50, number: 'b123456789' },
+    { banknote: 'ruble', nominal: 100, number: 'c123456789' },
+    { banknote: 'dollar', nominal: 100, number: 'd123456789' },
+    { banknote: 'dollar', nominal: 50, number: 'e123456789' },
+    { banknote: 'ruble', nominal: 100, number: 'f123456789' },
+    { banknote: 'dollar', nominal: 50, number: 'j123456789' },
+    { banknote: 'ruble', nominal: 50, number: 'h123456789' },
+  ]);
+
+  let currentMoney = money;
+
+  const [filter, setFilter] = useState('all');
+  // let currentMoney = money.filter(filteredMoney => filteredMoney.banknote === 'ruble' );
+  if (filter === 'ruble') {
+    currentMoney = money.filter(
+      (filteredMoney) => filteredMoney.banknote === 'ruble'
+    );
+  }
+  if (filter === 'dollar') {
+    currentMoney = money.filter(
+      (filteredMoney) => filteredMoney.banknote === 'dollar'
+    );
+  }
+
+  const onClickFilterHandler = (nameButton: string) => {
+    setFilter(nameButton);
+  };
   return (
+    <div>
+      <ul>
+        {currentMoney.map((objFromMoneyArr, index) => {
+          return (
+            <li key={index}>
+              <span>{objFromMoneyArr.banknote}</span>
+              <span>{objFromMoneyArr.nominal}</span>
+              <span>{objFromMoneyArr.number}</span>
+            </li>
+          );
+        })}
+      </ul>
+      <div style={{ marginLeft: '35px' }}>
+        <button onClick={() => onClickFilterHandler('all')}>all</button>
+        <button onClick={() => onClickFilterHandler('ruble')}>rubles</button>
+        <button onClick={() => onClickFilterHandler('dollar')}>dollars</button>
+      </div>
+    </div>
+  );
+  console.log();
+
+  /* return (
     <div>
       <h1>Hello {name}!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
-  );
+  ); */
 };
